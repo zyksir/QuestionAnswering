0e14d0ea4c6d2c3e7c9ded0fe67e46cdfc626099
zyksir <1600012810@pku.edu.cn>
2019-11-27 09:07:39 +0800

    .

--- code/translater.py
+++ code/translater.py
@@ -6,15 +6,22 @@
 '''
 import jieba
 import pickle
+from tqdm import tqdm
 
-train_file = ""
-valid_file = ""
-new_train_file = ""
-new_valid_file = ""
+
+train_file = "./data/train-set.data"
+valid_file = "./data/validation-set.data"
+new_train_file = "./data/train.pkl"
+new_valid_file = "./data/valid.pkl"
 word2id = {}
 id2word = {}
 sentences = set()
 sentences2ids = {}
+
+for word in ["[PAD]", "[START]", "[END]"]:
+    new_id = len(word2id)
+    word2id[word] = new_id
+    id2word[new_id] = word
 
 with open(train_file, "r") as f:
     for line in f:
@@ -28,7 +35,7 @@ with open(valid_file, "r") as f:
         sentences.add(line[0])
         sentences.add(line[1])
 
-for sentence in sentences:
+for sentence in tqdm(sentences):
     words = jieba.cut_for_search(sentence)
     for word in words:
         if word not in word2id:
@@ -37,14 +44,20 @@ for sentence in sentences:
             id2word[new_id] = word
     sentences2ids[sentence] = [word2id[word] for word in words]
 
-with open(train_file, "r") as f, open(new_train_file, "w") as fw:
+with open("./data/word2id.pkl", "wb") as fw:
+    pickle.dump(word2id, fw)
+
+with open("./data/id2word.pkl", "wb") as fw:
+    pickle.dump(id2word, fw)
+
+with open(train_file, "r") as f, open(new_train_file, "wb") as fw:
     train_triples = []
     for line in f:
         line = line.strip().split("\t")
         train_triples.append((sentences2ids[line[0]], sentences2ids[line[1]], int(line[2])))
     pickle.dump(train_triples, fw)
 
-with open(valid_file, "r") as f, open(new_valid_file, "w") as fw:
+with open(valid_file, "r") as f, open(new_valid_file, "wb") as fw:
     valid_triples = []
     for line in f:
         line = line.strip().split("\t")
